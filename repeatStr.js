72aa57937dc9b9b6c3432b18ce818fdd84410479
Ben663 <87435130+Ben663@users.noreply.github.com>
2022-10-08 00:45:07 +0300

    221007

---
+++ repeatStr.js
@@ -0,0 +1,7 @@
+// Write a function called repeat_str which repeats the given string src exactly count times.
+
+// function repeat_Str (string, num) {
+//     return string.repeat(6);
+    
+// }
+// console.log(repeat_Str(6, 'I'));
