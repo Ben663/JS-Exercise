4fbdf852a10f4e908decfa2d5cee9b959b34f41e
Ben663 <87435130+Ben663@users.noreply.github.com>
2022-10-07 17:17:27 +0300

    221007

--- yesorNo.js
+++ yesorNo.js
@@ -3,9 +3,9 @@ Ex1.1 - Yes or No
 Complete the method that takes a boolean value and 
 return a "Yes" string for true, or a "No" string for false. 
 */
-function yesorNo(val) 
+function yesorNo(value) 
 {
-    return val ? "YES" : "NO" ;
+    return value ? "YES" : "NO" ;
 }
 console.log(yesorNo(true)); //
 console.log(yesorNo(false));
