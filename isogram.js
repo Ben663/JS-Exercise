036b11953b618a338896b08300664428fdce2f3e
Ben663 <87435130+Ben663@users.noreply.github.com>
2022-10-10 18:12:54 +0300

    221010

--- isogram.js
+++ isogram.js
@@ -3,3 +3,19 @@ An isogram is a word that has no repeating letters, consecutive or non-consecuti
 a function that determines whether a string that contains only letters is an isogram. Assume the 
 empty string is an isogram. Ignore letter case.
 */
+
+function isIsogram (string) {
+    str = string.toLowerCase();
+
+    for (let i = 0; i < string.length - 1; i++) {
+        for (let j = i +1; j < string.length; j++) {
+            if (string[i] === string[j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+console.log(isIsogram("moOse"));
+console.log(isIsogram("aba")); // return false
+console.log(isIsogram("Dermatoglyphics")); // return true
--- organizeStr.js
+++ organizeStr.js
@@ -3,3 +3,19 @@ Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted 
 longest possible, containing distinct letters, 
 each taken only once - coming from s1 or s2.
 */
+
+
+function longest(s1, s2) {
+    let Arr = s1.toLowerCase().split("").concat(s2.toLowerCase().split(" "));
+    const newarr = [];
+    for (let i = 0; i < Arr.length; i++) {
+        if (!newarr.includes(Arr[i])) {
+            newarr.push(Arr[i])
+        }
+    }
+    return newarr.sort().join("");
+
+
+}
+console.log(longest("xyaabbbccccdefww", "xxxxyyyyabklmopq ", "hhhhhhyyyyabklmopq "));
+ // return abcdefwxxxxxyyyyabklmopqy
